407647985dab149c9d0ce617516049b272ff0ce7
jippeS <jippedominicussilvius@gmail.com>
2023-06-01 10:42:35 +0200

    python scripts

---
+++ versions/3.0/python_scripts/snakemake_report.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import configparser
+import argparse
+import sys
+import os
+
+
+class PreDemux:
+    def __init__(self):
+        args = argparser()
+        self.inputdir = args.inputdir
+
+    def Formatting_contents(self):
+        file_text = ""
+        with open(f"{self.inputdir}reports/Pipeline_execution.txt", 'r') as pipeline_file:
+            report = pipeline_file.read().split("@#")
+            g = []
+            for r in report:
+                if r.strip().startswith("qiime"):
+                    g.append(r.split("   "))
+            print(g)
+            # report_list = [report.split("  ") for report in pipeline_file.read().split("@#") if report.strip().startswith("qiime")]
+        sys.exit()
+        print(report_list)
+        for command in report_list:
+            command_text = ""
+            for line in command:
+                line = line.strip()
+                if not line.startswith("--"):
+                    command_text += f"{line}\n"
+                else:
+                    command_text += f"\t{line}\n"
+            file_text += f"{command_text}\n"
+        print(file_text)
+        # report_output_file(file_text, f"{self.inputdir}reports/Qiime_report.txt")
+
+    def Formatting_timestamps(self):
+        folder_path = f"{self.inputdir}reports/"  # Replace with the actual folder path
+
+        file_names = []  # To store the file names
+
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_names.append(file_path)
+
+        # Printing the complete file paths
+        for file_path in file_names:
+            print(file_path)
+
+        # Printing only the file names
+        for file_path in file_names:
+            file_name = os.path.basename(file_path)
+            print(file_name)
+
+def report_output_file(report, output_file):
+        """
+        Function for writing to files.
+        :param output_file: name of the file
+        :param report: The sentence or complete input that needs to be written to a file.
+        :param name: The name were the report should be written to.
+        """
+        if os.path.isfile(output_file):
+            with open(output_file, 'a') as file:
+                file.write(report)
+                file.close()
+        else:
+            with open(output_file, "w+") as file:
+                file.write(report)
+                file.close()
+def argparser():
+    """
+    Reads the arguments from the command line.
+    :return: command line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputdir", help="input file must be a fasta file.")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """
+    Execute the Class function in order.
+    """
+    pre_demux_calculation = PreDemux()
+    pre_demux_calculation.Formatting_contents()
+    #pre_demux_calculation.Formatting_timestamps()
+
+if __name__ == '__main__':
+    sys.exit(main())
