1ca84f163852e433464b8e633192f253587d6135
jippeS <jippedominicussilvius@gmail.com>
2023-06-06 18:04:53 +0200

    stuff

--- versions/3.0/bash_scripts/calc/trim_paired.sh
+++ versions/3.0/bash_scripts/calc/trim_paired.sh
@@ -3,6 +3,6 @@
 #SBATCH --job-name=trim_paired
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=16
 
-qiime cutadapt trim-paired --i-demultiplexed-sequences $1 --p-front-f $2 --p-front-r $3 --p-discard-untrimmed --o-trimmed-sequences $4
+qiime cutadapt trim-paired --i-demultiplexed-sequences $1 --p-front-f $2 --p-front-r $3 --p-discard-untrimmed --o-trimmed-sequences $4 --p-cores $5
