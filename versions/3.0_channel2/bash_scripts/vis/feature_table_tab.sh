49aad7319d18aed4b83134f3fc72bcf07daa2822
jippeS <jippedominicussilvius@gmail.com>
2026-08-17 13:31:34 +0000

    3.0 with multiple channel

---
+++ versions/3.0_channel2/bash_scripts/vis/feature_table_tab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --output=Tabulate_feature_table_%j.out
+#SBATCH --job-name=feature_table_tabulate
+#SBATCH --partition=Bytesflex
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+
+qiime feature-table tabulate-seqs --i-data $1 --o-visualization $2
